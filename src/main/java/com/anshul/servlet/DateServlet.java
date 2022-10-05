2942f391a389864edb7d3222a3532c30cbc51c69
Anshul <anshul.g2021@gmail.com>
2022-10-05 21:33:05 +0530

    Project initial commit

---
+++ src/main/java/com/anshul/servlet/DateServlet.java
@@ -0,0 +1,26 @@
+package com.anshul.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+@WebServlet("/date")
+public class DateServlet extends HttpServlet {
+
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		// set response content type
+		res.setContentType("text/html");
+		// Get PrintWriter object
+		PrintWriter pw = res.getWriter();
+		// write response
+		Date d = new Date();
+		pw.println("<b><i><center>Date and Time is" + d + "</b></i></center>");
+		// close stream
+		pw.close();
+	}// service(-,-)
+}// class
