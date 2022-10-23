94fc457dff8d6968b4fadd5a108e1b272dfac040
Anshul <anshul.g2021@gmail.com>
2022-10-23 11:41:41 +0530

    HIS-200: changes

--- src/main/java/com/anshul/servlet/DateServlet.java
+++ src/main/java/com/anshul/servlet/DateServlet.java
@@ -23,4 +23,8 @@ public class DateServlet extends HttpServlet {
 		// close stream
 		pw.close();
 	}// service(-,-)
+   //HIS-200 related changes
+  public void doProcess(){
+   //logic goes here
+  }
 }// class
